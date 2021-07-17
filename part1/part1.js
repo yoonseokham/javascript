57be55e0b4d5645ff0fa827fadc698583d9a168f
ha-mulan <ham5312@naver.com>
2021-07-17 15:36:28 +0900

    gitignore

--- part1/part1.js
+++ part1/part1.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
     'use strict';
+    paper.install(window)
+    paper.setup(document.getElementById('mainCanvas'))
+    paper.view.draw()
     console.log("part1.js loaded")
 })
