867a5738c0c473d68ed532b5f9e403cd505bf77f
yoonseok <ham5312@bearrobotics.ai>
2021-09-05 15:14:59 +0900

    init

--- part1/part1.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    'use strict';
-    paper.install(window)
-    paper.setup(document.getElementById('mainCanvas'))
-    paper.view.draw()
-    console.log("part1.js loaded")
-})
